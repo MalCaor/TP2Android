27a798525ef8ddd34496aefc53d61606ffe941ee
MalCaor <xav.lemen@orange.fr>
2019-12-11 14:38:37 +0100

    debut TP

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // LES OVERRIDES DE METHODES
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
