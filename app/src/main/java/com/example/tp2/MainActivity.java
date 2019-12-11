d62ed618c840eede40d4bb8816bef4a2e608e72f
MalCaor <xav.lemen@orange.fr>
2019-12-11 14:48:22 +0100

    ajout des overrides

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -3,13 +3,52 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    // LES OVERRIDES DE METHODES
+    // ### LES OVERRIDES DE METHODES ###
+    // # ON CREATE #
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w("P2","onCreate");
+    }
+    // # ON START #
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.w("P2","onStart");
+    }
+    // # ON RESTART #
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.w("P2","onRestart");
+    }
+    // # ON RESUME #
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.w("P2","onResume");
+    }
+    // # ON PAUSE #
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.w("P2","onPause");
+    }
+    // # ON STOP #
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.w("P2","onStop");
+    }
+    // # ON DESTROY #
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.w("P2","onDestroy");
     }
 }
