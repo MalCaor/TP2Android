a979666534e6f6598cb8cdcf1374725378755aa3
MalCaor <xav.lemen@orange.fr>
2019-12-13 08:47:22 +0100

    end TP

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// Import Bundle and Log
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+// Import for Button
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ### LES VARS ###
+    // # val Random #
+    private Random r;
+    private int choix, valeurMin = 1, valeurMax = 100;
 
     // ### LES OVERRIDES DE METHODES ###
     // # ON CREATE #
@@ -13,7 +25,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // send Log message
         Log.w("P2","onCreate");
+
+        // Button GALEA
+        Button galea=(Button) findViewById(R.id.gAlea);
+        galea.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                r = new Random();
+                choix = valeurMin + r.nextInt(valeurMax - valeurMin+1);
+            }
+        });
+        // Button AALEA
+        Button aalea=(Button) findViewById(R.id.aAlea);
+        aalea.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(
+                        getApplicationContext(),
+                        String.valueOf(choix),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
     // # ON START #
     @Override
@@ -55,12 +90,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("choix", choix);
         Log.w("P2","onSaveInstanceState");
     }
     // # ON RESTORE INSTANCE STATE #
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState){
         super.onRestoreInstanceState(savedInstanceState);
+        choix = savedInstanceState.getInt("choix");
         Log.w("P2","onRestoreInstanceState");
     }
 }
